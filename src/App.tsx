2393e376845c6497c61e6c483204a1bab9d5bd97
zktm9903 <zktm9903@naver.com>
2022-08-29 15:09:03 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Header from 'components/Header';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className="fixed top-0 left-0 right-0 flex justify-center items-center h-11 bg-black opacity-80 text-white">
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+    </nav>
+  );
+}
+
+export default Header;
